ff9e10594cddf3052f66c37758bf35e2524f1e8a
SallyResch <sallyresch27@gmail.com>
2026-02-04 15:43:17 +0100

    added sidebar and main zoo div and content component

--- routes/zoo.js
+++ routes/zoo.js
@@ -1,6 +1,6 @@
 import express from "express";
 import * as path from "path";
-//import { birdBreeds } from "../data/birdBreeds.js";
+import animals from "../data/animals.js"
 const zooRouter = express.Router()
 const __dirname = path.resolve();
 
@@ -8,12 +8,13 @@ const __dirname = path.resolve();
 zooRouter.get("/", (req, res) => {
     res.render(path.join(__dirname, "/views/pages/zoo.ejs"),
         {
-            title: "Birds",
+            title: "Zoo",
             image: "shoeBill",
             className: "birdBody",
-            headerTitle: "Birds",
-            subtitle: "Subtitle from headerComponent",
-            docTitle: "Birds"
+            headerTitle: "Australian Zoo",
+            subtitle: "Zoo Animals",
+            docTitle: "Birds",
+            zooData: animals
         }
     );
 })
